cc2b119e328faa4df3ce22a84d6ce242deeaad4e
Daniil Yakubov <daniil-yak@yandex.ru>
2023-07-31 18:31:55 +0300

    fix: текст теперь не выходит за границы

--- src/app/todolist/components/TaskComponents.tsx
+++ src/app/todolist/components/TaskComponents.tsx
@@ -1,14 +1,7 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import CheckIcon from '@mui/icons-material/Check';
-import {
-	Box,
-	Divider,
-	IconButton,
-	Paper,
-	TextField,
-	Typography,
-} from '@mui/material';
+import { Box, Divider, IconButton, Paper, TextField } from '@mui/material';
 import { useState } from 'react';
 import { Task } from '../const/const';
 
@@ -27,79 +20,67 @@ const TaskComponent = (props: TaskComponentProps) => {
 	};
 
 	return (
-		<div>
-			{edit ? (
-				<Paper
-					component="form"
-					sx={{
-						m: '16px 8px',
-						p: '0px 8px',
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'space-between',
-					}}
-				>
+		<Paper
+			component="form"
+			sx={{
+				m: '16px 8px',
+			}}
+		>
+			<Box
+				sx={{
+					display: 'flex',
+					alignItems: 'center',
+				}}
+			>
+				{edit ? (
 					<TextField
 						label="Редактировать задачу"
 						variant="outlined"
 						multiline
 						fullWidth
+						inputProps={{ maxLength: 100 }}
 						value={input}
 						onChange={(e) => setInput(e.target.value)}
 					/>
-
-					<Box
-						sx={{
-							display: 'flex',
-							alignItems: 'center',
+				) : (
+					<TextField
+						variant="outlined"
+						multiline
+						fullWidth
+						InputProps={{
+							readOnly: true,
 						}}
-					>
+						value={props.data.title}
+					/>
+				)}
+
+				<Box
+					sx={{
+						display: 'flex',
+						alignItems: 'center',
+					}}
+				>
+					{edit ? (
 						<IconButton
 							sx={{ m: 1 }}
 							onClick={() => props.onEdit(props.idInArray, input)}
 						>
 							<CheckIcon />
 						</IconButton>
-
-						<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-
-						<IconButton sx={{ m: 1 }} onClick={props.onDelete}>
-							<DeleteIcon />
-						</IconButton>
-					</Box>
-				</Paper>
-			) : (
-				<Paper
-					component="form"
-					sx={{
-						m: '16px 8px',
-						p: '0px 8px',
-						display: 'flex',
-						alignItems: 'center',
-						justifyContent: 'space-between',
-					}}
-				>
-					<Typography sx={{ m: 1 }}>{props.data.title}</Typography>
-
-					<Box
-						sx={{
-							display: 'flex',
-							alignItems: 'center',
-						}}
-					>
+					) : (
 						<IconButton sx={{ m: 1 }} onClick={() => editTask()}>
 							<EditIcon />
 						</IconButton>
+					)}
 
-						<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+					<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
 
-						<IconButton sx={{ m: 1 }} onClick={props.onDelete}>
-							<DeleteIcon />
-						</IconButton>
-					</Box>
-				</Paper>
-			)}
-		</div>
+					<IconButton sx={{ m: 1 }} onClick={props.onDelete}>
+						<DeleteIcon />
+					</IconButton>
+				</Box>
+			</Box>
+		</Paper>
 	);
 };
 
