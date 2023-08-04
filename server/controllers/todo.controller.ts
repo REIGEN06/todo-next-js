abd9d78ebac3b638f81d9026f22cab521fea2ed2
Daniil Yakubov <daniil-yak@yandex.ru>
2023-08-04 23:46:12 +0300

    feat: типизация, zod

--- server/controllers/todo.controller.ts
+++ server/controllers/todo.controller.ts
@@ -1,32 +1,41 @@
+import { Request, Response } from 'express';
+import { z } from 'zod';
 const database = require('../models/index');
+
+const taskSchema = z.object({
+	title: z.string().optional(),
+	id: z.string().optional(),
+});
+
 class ToDoController {
-	async createTask(req: any, res: any) {
-		const { title } = req.body;
+	async createTask(req: Request, res: Response) {
+		const reqBody = taskSchema.parse(req.body);
 		const newTask = await database.Todo.create({
-			title: title,
+			title: reqBody.title,
 		});
 		res.json(newTask.id);
 	}
-	async deleteTask(req: any, res: any) {
+	async deleteTask(req: Request, res: Response) {
+		const reqParams = taskSchema.parse(req.params);
 		database.Todo.destroy({
 			where: {
-				id: req.params.id,
+				id: reqParams.id,
 			},
 		});
 	}
-	async editTask(req: any, res: any) {
-		console.log(req.body.title);
-
+	async editTask(req: Request, res: Response) {
+		const reqParams = taskSchema.parse(req.params);
+		const reqBody = taskSchema.parse(req.body);
 		database.Todo.update(
-			{ title: req.body.title },
+			{ title: reqBody.title },
 			{
 				where: {
-					id: req.params.id,
+					id: reqParams.id,
 				},
 			}
 		);
 	}
-	async getTasks(req: any, res: any) {
+	async getTasks(req: Request, res: Response) {
 		const result = await database.Todo.findAll({ raw: true });
 		res.json(result);
 	}
--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,18 @@
 const express = require('express');
 const todoRouter = require('./routes/todo.routes');
 const PORT = process.env.PORT || 8080;
-
+import { Request, Response, NextFunction } from 'express';
 const app = express();
 
 app.use(express.json());
-app.use(function (req: any, res: any, next: () => void) {
-	res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req: Request, res: Response, next: NextFunction) {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 	res.header(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept'
 	);
 	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE,PATCH');
-	res.header('Access-Control-Allow-Credentials', true);
+
 	if ('OPTIONS' == req.method) {
 		res.sendStatus(200);
 	} else {
