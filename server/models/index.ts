b50e256d469956aa8f7e7e573bffb245dd336e5c
Daniil Yakubov <daniil-yak@yandex.ru>
2023-08-13 10:52:43 +0300

    fix: config теперь в gitignore; удаление мусора

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,23 +1,22 @@
 'use strict';
 
 require('dotenv').config();
-const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV;
 const config = require(__dirname + '/../config/config.json')[env];
 const basename = path.basename(__filename);
 
-const db: dbType = {
-	sequelize: '',
-	Sequelize: '',
-	Todo: '',
-};
-
 type dbType = {
 	sequelize: string;
 	Sequelize: string;
 	Todo: string;
+};
+
+const db: dbType = {
+	sequelize: '',
+	Sequelize: '',
+	Todo: '',
 };
 
 let sequelize;
@@ -31,29 +30,6 @@ if (config.use_env_variable) {
 		config
 	);
 }
-
-fs.readdirSync(__dirname)
-	.filter((file) => {
-		return (
-			file.indexOf('.') !== 0 &&
-			file !== basename &&
-			file.slice(-3) === '.js' &&
-			file.indexOf('.test.js') === -1
-		);
-	})
-	.forEach((file) => {
-		const model = require(path.join(__dirname, file))(
-			sequelize,
-			Sequelize.DataTypes
-		);
-		db[model.name] = model;
-	});
-
-Object.keys(db).forEach((modelName) => {
-	if (db[modelName].associate) {
-		db[modelName].associate(db);
-	}
-});
 
 const Todo = require('./todo')(sequelize, Sequelize.DataTypes);
 
